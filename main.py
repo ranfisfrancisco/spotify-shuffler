d9aa1cf95f666955544befa01cb772b74bcd2217
Ranfis Francisco <ranfis.francisco@gmail.com>
2022-02-05 16:37:57 -0500

    able to queue

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import enum
 import sys
 import spotipy
 import spotipy.util as util
@@ -11,10 +12,52 @@ def get_auth(username, scope):
 
     sp = spotipy.Spotify(auth=token)
     return sp
+
+def select_playlist(playlists):
+    # Note: User is choosing with 1 based indexes.
+    for idx, item in enumerate(playlists):
+        print(f'Index {idx+1} | Name {item["name"]} | Tracks {item["tracks"]["total"]} ')
+
+    entry = None
+    select_flag = 0
+    while select_flag == 0:
+        entry = input('Select Playlist by Index or Name?\n1. Index\n2. Name\n')
+        if entry.isnumeric() and int(entry) == 1:
+            select_flag = 1
+        elif entry.isnumeric() and int(entry) == 2:
+            select_flag = 2
+        else:
+            print("Invalid Input")
     
+
+    entry = None
+    if select_flag == 1:
+        while True:
+            entry = input('Enter index: ')
+            if not entry.isnumeric():
+                continue
+
+            idx = int(entry)
+            if idx > len(playlists) or idx < 1:
+                print("Invalid Index")
+                continue
+            
+            return playlists[idx+1]
+
+    if select_flag == 2:
+        while True:
+            entry = input('Enter name: ')
+
+            for item in playlists:
+                if item['name'] == entry:
+                    return item
+
+            print("Did not find name!")
+            
+
 load_dotenv()
 
-scope = 'user-library-read user-read-recently-played'
+scope = 'user-library-read user-read-recently-played playlist-read-private streaming'
 
 if len(sys.argv) > 1:
     username = sys.argv[1]
@@ -31,7 +74,7 @@ if not sp:
 results = sp.current_user_recently_played(limit=50)
 recent_track_list = results['items']
 
-# print(recent_track_list[0]['tracks].keys()) 
+# print(recent_track_list[0]['tracks'].keys()) 
 ''' dict_keys(['album', 'artists', 'available_markets',
 'disc_number', 'duration_ms', 'explicit', 'external_ids', 'external_urls', 'href', 'id', 'is_local', 'name', 'popularity', 'preview_url', 'track_number', 'type', 'uri'])'''
 
@@ -44,8 +87,15 @@ playlists = results['items']
 # for item in playlists:
 #     print(item['name'], item['id'], item['tracks']['total'])
 
-results = sp.user_playlist_tracks(playlist_id=playlists[0]['id'])
-x = results['items']
+# Select Playlist
+playlist = select_playlist(playlists)
 
-for item in x:
-    print(item['track']['name'])
+# Get Tracks of Selected Playlist
+results = sp.user_playlist_tracks(playlist_id=playlist['id'])
+playlist_tracks = results['items']
+
+# for item in playlist_tracks:
+#     print(item['track']['name'])
+
+# Queue
+sp.add_to_queue(playlist_tracks[0]['track']['uri'])
